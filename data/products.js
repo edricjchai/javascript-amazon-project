1269d04bdb9b2b06ad4b7f96f92380faf2481ce3
Edric Chai <edricchai@gmail.com>
2024-05-02 08:48:44 -0400

    Add backend practice file

--- data/products.js
+++ data/products.js
@@ -58,6 +58,13 @@ class Clothing extends Product{
   }
 }
 
+/*
+//Built-in classes
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
